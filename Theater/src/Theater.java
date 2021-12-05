36bde1c317df7641b68a6a8b5055cf4c9d96a296
chuncheng1 <94091732+chuncheng1@users.noreply.github.com>
2021-12-05 00:51:37 -0500

    Add files via upload

--- Theater/src/Theater.java
+++ Theater/src/Theater.java
@@ -4,8 +4,10 @@ public class Theater {
 	private int capacity;
 	private Seat[][] seats;
 	private Showtime[] movieSchedule;
+	private int showTimeNum;
 	private static int SEAT_ROWS = 5;
 	private static int SEAT_COLS = 5;
+	private static int MAX_SHOWTIME_COUNT = 21;
 	
 	public Theater(int id) {
 		ID = id;
@@ -20,7 +22,8 @@ public class Theater {
 			}
 		}
 		
-		movieSchedule = new Showtime[21];
+		movieSchedule = new Showtime[MAX_SHOWTIME_COUNT];
+		showTimeNum = 0;
 	}
 	
 	public int getID() {
@@ -39,10 +42,69 @@ public class Theater {
 		return movieSchedule;
 	}
 	
-	public void listSeats() {
-		// method to list all the seats in the theater
-		// seats listed should have seatID if not taken, "X" if taken
+	//adds showtime s, returns true if successfully added, false if movieschedule is full or if
+	//the time slot is already occupied
+	public boolean addShowTime(Showtime s) {
+		if (showTimeNum == MAX_SHOWTIME_COUNT) {
+			System.out.println("Max showtime reached.");
+			return false;
+		}
+		
+		Time st = s.getTime();
+		for (int i = 0; i < MAX_SHOWTIME_COUNT; i++) {
+			if (movieSchedule[i] != null) {
+				Time currentTime = movieSchedule[i].getTime();
+				if (currentTime.equals(st)) {
+					System.out.println("Time slot already has a show.");
+					return false;
+				}
+			}
+		}
+		movieSchedule[showTimeNum] = s;
+		showTimeNum++;
+		return true;
 	}
 	
+	//deletes showtime s, returns true if successfully deleted, false if the showtime is not in the schedule
+	public boolean deleteShowTime(Showtime s) {
+		Time st = s.getTime();
+		for(int i = 0; i < MAX_SHOWTIME_COUNT; i++) {
+			if (movieSchedule[i] != null) {
+				Time currentTime = movieSchedule[i].getTime();
+				if (currentTime.equals(st)) {
+					int j = i;
+					while (j < MAX_SHOWTIME_COUNT-1) {
+						movieSchedule[j] = movieSchedule[j+1];
+						j++;
+					}
+					movieSchedule[j] = null;
+					showTimeNum--;
+					return true;
+				}
+			}
+		}
+		System.out.println("Showtime not found.");
+		return false;
+	}
+	
+	//printSeats - lists all the seats in the theater at time t, taken seats are shown as "X,"
+	// available seats are shown as seatID
+	public void printSeats(Time t) {
+		for (int i = 0; i < SEAT_ROWS; i++) {
+			for (int j = 0; j < SEAT_COLS; j++) {
+				if (seats[i][j].searchReservation(t) == true) {
+					System.out.print(" X");
+				} else {
+					int id = seats[i][j].getID();
+					if (id < 10) {
+						System.out.print(" ");
+					}
+					System.out.print(id);
+				}
+				System.out.print(" | ");
+			}
+			System.out.println();
+		} 
+	}
 }
 
