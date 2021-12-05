ef7369ac49ebda900861263178082173671cdddd
sammy <samunger@bu.edu>
2021-12-05 18:31:49 -0500

    implemented first 3 main method options

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,24 +1,24 @@
-public class UserInterface {
-    public static void main(String[] args) {
-        Database.Theater theater1 = new Database.Theater(1);
-        Database.Theater theater2 = new Database.Theater(2);
-        Database.Theater theater3 = new Database.Theater(3);
-        Database.Theater theater4 = new Database.Theater(4);
-        Database.Theater theater5 = new Database.Theater(5);
-        Database.Theater theater6 = new Database.Theater(6);
-
-        Database.Movie movie1 = new Database.Movie("Encanto", "11/24/2021", "Musical", "PG", "1:49");
-        Database.Movie movie2 = new Database.Movie("House of Gucci", "11/24/2021", "Drama", "R", "2:37");
-        Database.Movie movie3 = new Database.Movie("Resident Evil: Welcome to Raccoon City", "11/24/2021", "Horror", "R", "1:47");
-        Database.Movie movie4 = new Database.Movie("Ghostbusters: Afterlife", "11/19/2021", "Sci-Fi", "PG-13", "2:04");
-        Database.Movie movie5 = new Database.Movie("King Richard", "11/19/2021", "Drama", "PG-13", "2:24");
-        Database.Movie movie6 = new Database.Movie("Eternals", "11/05/2021", "SuperHero", "PG-13", "2:37");
-
-
-        Database.Ticket example = new Database.Ticket("G5", "4:30", "12/10", "Eternals",1);
-        System.out.println("Welcome!");
-
-//        System.out.println(movie2.Date);
-//        System.out.println(theater1.SeatSlots[0][0][0]);
-    }
-}
+//public class UserInterface {
+//    public static void main(String[] args) {
+//        Database.Theater theater1 = new Database.Theater(1);
+//        Database.Theater theater2 = new Database.Theater(2);
+//        Database.Theater theater3 = new Database.Theater(3);
+//        Database.Theater theater4 = new Database.Theater(4);
+//        Database.Theater theater5 = new Database.Theater(5);
+//        Database.Theater theater6 = new Database.Theater(6);
+//
+//        Database.Movie movie1 = new Database.Movie("Encanto", "11/24/2021", "Musical", "PG", "1:49");
+//        Database.Movie movie2 = new Database.Movie("House of Gucci", "11/24/2021", "Drama", "R", "2:37");
+//        Database.Movie movie3 = new Database.Movie("Resident Evil: Welcome to Raccoon City", "11/24/2021", "Horror", "R", "1:47");
+//        Database.Movie movie4 = new Database.Movie("Ghostbusters: Afterlife", "11/19/2021", "Sci-Fi", "PG-13", "2:04");
+//        Database.Movie movie5 = new Database.Movie("King Richard", "11/19/2021", "Drama", "PG-13", "2:24");
+//        Database.Movie movie6 = new Database.Movie("Eternals", "11/05/2021", "SuperHero", "PG-13", "2:37");
+//
+//
+//        Database.Ticket example = new Database.Ticket("G5", "4:30", "12/10", "Eternals",1);
+//        System.out.println("Welcome!");
+//
+////        System.out.println(movie2.Date);
+////        System.out.println(theater1.SeatSlots[0][0][0]);
+//    }
+//}
